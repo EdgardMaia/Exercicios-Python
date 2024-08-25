b707e299045890448c3a4af70d9a68b1dcd8b287
EdgardMaia <edgard_maia@outlook.com>
2024-08-25 17:10:31 -0300

    5° Questão

---
+++ Estrutura_Sequencial/ex_005.py
@@ -0,0 +1,8 @@
+#Faça um Programa que converta metros para centímetros.
+
+metros = float(input("Digite o tamanho em metros: "))
+
+conversor = metros*100
+
+print(f"Convertendo de metros para centimetros o seu resultado é: {conversor}")
+
