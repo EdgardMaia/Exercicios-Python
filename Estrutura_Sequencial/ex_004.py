e6e5f60afbf55485542b73b2fa7c55b670156943
EdgardMaia <edgard_maia@outlook.com>
2024-08-25 16:47:23 -0300

    4° Questão

---
+++ Estrutura_Sequencial/ex_004.py
@@ -0,0 +1,11 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+nota1 = int(input("Digite o primeiro numero: "))
+nota2 = int(input("Digite o segundo numero: "))
+nota3 = int(input("Digite o terceiro numero: "))
+nota4 = int(input("Digite o quarto numero: "))
+
+media = (nota1+nota2+nota3+nota4)/4
+
+print(f"O resultado da media é: {media}")
+
