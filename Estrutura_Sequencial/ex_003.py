7319c07c0ae5ee28f9105f13c5e69b1df8c6aa04
EdgardMaia <edgard_maia@outlook.com>
2024-08-25 16:18:21 -0300

    3° Questão

---
+++ Estrutura_Sequencial/ex_003.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça dois números e imprima a soma
+
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+soma = num1 + num2
+
+print(f"Resultado da soma: {soma}")
