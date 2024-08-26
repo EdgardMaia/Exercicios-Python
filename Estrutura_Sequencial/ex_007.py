eff83907bb1bc40ae5643ce7956d715ecce08d5e
EdgardMaia <edgard_maia@outlook.com>
2024-08-26 17:31:42 -0300

    7ª Questão

---
+++ Estrutura_Sequencial/ex_007.py
@@ -0,0 +1,6 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+lado_quadrado = float(input("Digite um lado do quadrado: "))
+area_quadrado = lado_quadrado**2
+dobro_area  = area_quadrado * 2
+print(f"O do dobro da area do quadrado {area_quadrado} é de {dobro_area}.")
