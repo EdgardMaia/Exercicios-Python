405f0b664086135cb6a5832f659459168746aca1
EdgardMaia <edgard_maia@outlook.com>
2024-08-26 15:50:21 -0300

    6ª Questão

---
+++ Estrutura_Sequencial/ex_006.py
@@ -0,0 +1,5 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. 
+
+raio_circulo = float(input("Digite o tamanho do raio do circulo: "))
+area = 3.14*(raio_circulo)**2
+print(f"O raio de {raio_circulo} equivale a uma area de {area}.")
