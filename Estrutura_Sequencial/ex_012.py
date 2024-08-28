ca14b25d87af8ed16cd8de43ce86c378a0156bd9
EdgardMaia <edgard_maia@outlook.com>
2024-08-28 19:51:27 -0300

    12ª Questão

---
+++ Estrutura_Sequencial/ex_012.py
@@ -0,0 +1,8 @@
+#Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+
+altura = float(input("Digite sua altura: "))
+calculo = (72.7*altura) - 58
+print(f"Seu peso ideal seria de: {calculo}Kg")
+
+
+
